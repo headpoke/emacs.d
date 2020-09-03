4271f1c76f544f8ad7b659688109e2e758366433
Knut Berg <headpoke@gmail.com>
2020-09-03 23:22:00 +0200

    .

---
+++ userinfo.el
@@ -0,0 +1,6 @@
+;;; user-info.el --- -*- lexical-binding: t -*-
+(setq user-full-name         "Knut Berg"
+      user-mail-address      "knut.berg@nord.no"
+      calendar-latitude      67.289
+      calendar-longitude     14.560
+      calendar-location-name "Bodø, Norway")
